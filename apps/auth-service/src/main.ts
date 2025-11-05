c8d8514087a3e58d0defb98696b2364cd7697278
Aditya Rai <adityarai29052005@gmail.com>
2025-11-05 15:06:55 +0530

    feat: Initial setup of auth-service and api-gateway

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,30 @@
 import express from 'express';
+import cors from "cors";
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+// const host = process.env.HOST ?? 'localhost';
+// const port = process.env.PORT ? Number(process.env.PORT) : 6002;
 
 const app = express();
+
+app.use(
+  cors(
+    { 
+      origin: 'http://localhost:3000',
+      allowedHeaders: ["Authorization", "Content_Type"],
+      credentials: true, 
+    }
+  )
+);
 
 app.get('/', (req, res) => {
     res.send({ 'message': 'Hello API'});
 });
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+const port = process.env.PORT || 6002;
+const server = app.listen(port,() => {
+    console.log(`Auth service running al http://localhost:${port}/api`)
 });
+
+server.on("error", (err) => {
+  console.log("Server Error:", err)
+})
